f478f86a7886b088c97b5daab1b66d4a021f1d86
Arvi <xarvichanx@gmail.com>
2023-03-02 18:48:48 +0100

    TimeManager
    Added time script for current minute, hour, day in the season and season

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public enum Season
+    {
+        Summer,
+        Winter,
+        Spring,
+        Fall
+    }
+
+    public Season currentSeason = Season.Summer;
+    public int dayNumber = 0;
+    public int hour;
+    public float minute;
+    public float timeSpeed;
+
+
+
+    private readonly Dictionary<Season, int> daysInSeason = new()
+    {
+        //My game, I set the rules how long a season is ;D
+        {Season.Summer, 30},
+        {Season.Winter, 20},
+        {Season.Spring, 30},
+        {Season.Fall, 35}
+    };
+
+    void Start()
+    {
+        hour = 0; minute = 0;
+    }
+
+    void Update()
+    {
+        CalculateGameTime();
+    }
+
+    private void CalculateGameTime()
+    {
+        if (minute >= 60f)
+        {
+            hour += 1;
+            minute = 0;
+        }
+        if (hour > 23)
+        {
+            dayNumber += 1;
+            hour = 0;
+        }
+        if (dayNumber > daysInSeason[currentSeason])
+        {
+            dayNumber = 0;
+            currentSeason = GetNextSeason();
+        }
+        minute += Time.deltaTime * timeSpeed;
+    }
+
+    private Season GetNextSeason()
+    {
+        
+        switch (currentSeason)
+        {
+            case Season.Summer:
+                return Season.Fall;
+            case Season.Fall:
+                return Season.Winter;
+            case Season.Winter:
+                return Season.Spring;
+            case Season.Spring:
+                return Season.Summer;
+            default:
+                return Season.Summer;
+        }
+    }
+
+}
